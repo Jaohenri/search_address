e2df2d74d05cb0f9e9debd661cd94432a2f5e245
Jaohenri <joaohrcf10@gmail.com>
2026-01-02 00:55:27 -0300

    Trying via cep api

--- cep/cep.py
+++ cep/cep.py
@@ -1,6 +1,16 @@
 import requests
 
-response = requests.get("https://api.github.com")
+while True:
+    cep = input("Please inform your cep: ")
+    if cep.isdigit():
+        break
+    print("Invalid cep, must be only numbers.")
+
+response = requests.get(f"https://viacep.com.br/ws/{cep}/json/").json()
 
 # Exibindo o conteúdo da resposta
-
+print(f"State: {response.get('uf')}\n"
+      f"City: {response.get('localidade')}\n"
+      f"Neighborhood: {response.get('bairro')}\n"
+      f"Street: {response.get('logradouro')}\n"
+      )
