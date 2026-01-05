232e180e6da3d4c91f155149bd4b3f4640936a39
Jaohenri <joaohrcf10@gmail.com>
2026-01-04 23:26:07 -0300

    adding some validations

--- cep/cep.py
+++ cep/cep.py
@@ -1,23 +1,31 @@
+
+
 import requests
 
 class Cep:
     def __init__(self, cep):
         self.cep = cep
         self.cleaned_cep = self.clean_cep()
+
         if self.cleaned_cep != None:
             self.response = self.search_cep()
-            self.state = self.response.get('uf')
-            self.city = self.response.get('localidade')
-            self.neighborhood = self.response.get('bairro')
-            self.street = self.response.get('logradouro')
-            self.display_cep()
+
+            if self.response.get("erro"):
+                print("The cep inputed doesn't exist.")
+            else:
+                self.state = self.response.get('uf')
+                self.city = self.response.get('localidade')
+                self.neighborhood = self.response.get('bairro')
+                self.street = self.response.get('logradouro')
+                
+                self.display_cep()
         
 
     def clean_cep(self):
-        if self.cep.isdigit():
+        if self.cep.isdigit() and self.cep.len() == 8:
             return self.cep
         else:
-            print("Invalid cep, must be only numbers.")
+            print("Invalid cep, must be 8 numbers.")
             return None
     
     def search_cep(self):
